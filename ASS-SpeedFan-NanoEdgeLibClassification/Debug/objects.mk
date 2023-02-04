75ac13610605332467f9e77a55d7b23197f1a6ed
ChiamHweeMin <tjin863@gmail.com>
2023-02-04 12:37:07 +0800

    Upload

---
+++ ASS-SpeedFan-NanoEdgeLibClassification/Debug/objects.mk
@@ -0,0 +1,9 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+USER_OBJS := C:/Users/60111/STM32CubeIDE/workspace_1.10.1/ASS-SpeedFan-NanoEdgeLibClassification/acc_lib/libneai.a
+
+LIBS :=
+
